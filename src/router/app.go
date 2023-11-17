4e722c67dd9e288b239d33f59f2923698d2b80cf
Haswell <haswell@HaswelldeMacBook-Air.local>
2023-11-17 11:23:51 +0800

    Hardware CRUD done

--- src/router/app.go
+++ src/router/app.go
@@ -15,11 +15,11 @@ func Router() *gin.Engine {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.GET("/precheck", service.Precheck)
 	r.POST("/init", service.Init)
-	r.POST("/user-detail", service.GetUserDetail)
+	r.GET("/user-detail", service.GetUserDetail)
 	r.POST("/login", service.Login)
 	r.POST("/register", service.Register)
 	authUser := r.Group("/user", middlewares.AuthUserCheck())
-	authUser.POST("/hdq", service.HardwareQuery)
+	authUser.GET("/hdq", service.HardwareListQuery)
 	authUser.GET("/hdcq", service.HardwareCategoryQuery)
 
 	return r
--- src/service/Hardware.go
+++ src/service/Hardware.go
@@ -49,7 +49,7 @@ func HardwareLocationQuery(c *gin.Context) {
 		"data": data,
 	})
 }
-func HardwareQuery(c *gin.Context) {
+func HardwareListQuery(c *gin.Context) {
 	name := struct {
 		HardwareName string
 		Category     string
@@ -61,7 +61,7 @@ func HardwareQuery(c *gin.Context) {
 		c.Status(http.StatusBadRequest)
 		return
 	}
-	query := dbc.DB().Where("hardware_name LIKE ?", "%"+name.HardwareName+"%")
+	query := dbc.DB().Model(&models.Hardware{}).Where("hardware_name LIKE ?", "%"+name.HardwareName+"%")
 	if name.Category != "" {
 		query = query.Where("Category = ?", name.Category)
 	}
@@ -90,5 +90,126 @@ func HardwareQuery(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"data": data,
 	})
+}
 
+func HardwareDetailQuery(c *gin.Context) {
+	req := struct {
+		HardwareID uint
+	}{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("Bad request")
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	var data1 []models.Hardware
+	query := dbc.DB().Model(&models.Hardware{}).Where("hardware_id = ?", req)
+	err := query.Find(&data1).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+	var data2 []models.HardwareMaintenance
+	query = dbc.DB().Model(&models.HardwareMaintenance{}).Where("hardware_id = ?", req)
+	err = query.Find(&data2).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": map[string]interface{}{
+			"HDinfo":    data1,
+			"Maintinfo": data2,
+		},
+	})
+}
+func HardwareUpdate(c *gin.Context) {
+	req := models.Hardware{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("Bad request")
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	query := dbc.DB().Model(&models.Hardware{}).Where("hardware_id = ?", req.HardwareID)
+	queryM := dbc.DB().Model(&models.Hardware{}).Where("hardware_id = ? and status = '待处理'", req.HardwareID)
+	var chk *models.HardwareMaintenance
+	if err := queryM.First(chk); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+
+	if set := map[string]bool{"保留": true, "正常": true, "占用": true}; set[req.Status] && chk != nil {
+		c.JSON(http.StatusFailedDependency, gin.H{
+			"msg": "Attempting to transform an abnormal hardware with maintenance processes unfinished to a normal one.",
+		})
+		return
+	}
+	if err := query.Updates(&req); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func HardwareDelete(c *gin.Context) {
+	req := struct {
+		HardwareID uint
+	}{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("Bad request")
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	query := dbc.DB().Model(&models.Hardware{}).Where("hardware_id = ?", req.HardwareID)
+	queryM := dbc.DB().Model(&models.Hardware{}).Where("hardware_id = ?", req.HardwareID)
+	var tgt models.Hardware
+	if err := query.Find(&tgt).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+	var chk *models.HardwareMaintenance
+	if err := queryM.First(chk).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+	if chk != nil {
+		c.JSON(http.StatusFailedDependency, gin.H{
+			"msg": "Attempting to remove a hardware with maintenance unfinished.",
+		})
+		return
+	}
+	if err := dbc.DB().Delete(&tgt).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func HardwareCreate(c *gin.Context) {
+	req := models.Hardware{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("Bad request")
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	if err := dbc.DB().Create(&req).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": err,
+		})
+		return
+	}
+	c.Status(http.StatusOK)
 }
