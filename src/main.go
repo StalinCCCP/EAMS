a6bef1f85baec8889fb3010cfc2cb1236ba56b5c
Haswell <haswell@HaswelldeMacBook-Air.local>
2023-11-19 20:26:49 +0800

    Minor changes

--- src/main.go
+++ src/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "EAMSbackend/router"
+import (
+	"EAMSbackend/router"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
+	//gin.DefaultWriter = io.Discard
 	r := router.Router()
 	r.Run()
 }
