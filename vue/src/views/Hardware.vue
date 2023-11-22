888c7d9a091ba3d9cf567047b2a48f987e9e5b21
mrlonely <116348059+mameikagou@users.noreply.github.com>
2023-11-22 15:56:26 +0800

    update the style of input

--- vue/src/views/Hardware.vue
+++ vue/src/views/Hardware.vue
@@ -1,73 +1,77 @@
 <template>
-    <n-space vertical :size="12">
-    <n-form
-    ref="formRef"
-    inline
-    :label-width="80"
-    :model="req"
-    :size="size"
-  >
-    <n-form-item >
-      <n-input v-model:value="req.HardwareName" placeholder="Find..." />
-    </n-form-item>
-    <n-form-item>
-      <n-button attr-type="button" @click="find">
-        Go!
-      </n-button>
-    </n-form-item>
-  </n-form>
+  <n-space vertical :size="12">
+    <n-form class="centerform" ref="formRef" inline :label-width="80" :model="req" :size="size">
+      <n-form-item>
+        <n-input size="large" v-model:value="req.HardwareName" placeholder="Find..." />
+      </n-form-item>
+      <n-form-item>
+        <n-button attr-type="button" @click="find"> Go! </n-button>
+      </n-form-item>
+    </n-form>
   </n-space>
-    <n-space vertical :size="12">
-      <n-data-table
-        :bordered="false"
-        :single-line="false"
-        :columns="columns"
-        :data="data"
-        :pagination="pagination"
-      />
-    </n-space>
-  </template>
+  <n-space vertical :size="12">
+    <n-data-table
+      :bordered="false"
+      :single-line="false"
+      :columns="columns"
+      :data="data"
+      :pagination="pagination"
+    />
+  </n-space>
+</template>
 <script setup>
-import {h,defineComponent, onMounted} from 'vue'
-import {NButton,useMessage,NDataTable,NSpace,NForm,NFormItem,NInput} from 'naive-ui'
+import { h, defineComponent, onMounted } from "vue";
+import {
+  NButton,
+  useMessage,
+  NDataTable,
+  NSpace,
+  NForm,
+  NFormItem,
+  NInput,
+} from "naive-ui";
 // import {DataTableColumns} from 'naive-ui'
-import http from '@/util/http';
-import { createToast } from 'mosha-vue-toastify';
-import createColumns from '@/models/HardwareTable'
-import {ref} from 'vue'
-const req=ref({
-    HardwareName:'',
-    Category:'',
-    Status:'',
-    Location:''
-})
-const data=ref([])
-const formRef = ref(null)
-const size =ref("medium")
+import http from "@/util/http";
+import { createToast } from "mosha-vue-toastify";
+import createColumns from "@/models/HardwareTable";
+import { ref } from "vue";
+const req = ref({
+  HardwareName: "",
+  Category: "",
+  Status: "",
+  Location: "",
+});
+const data = ref([]);
+const formRef = ref(null);
+const size = ref("medium");
 
-const post = ()=>{
-    http.post("/p/user/hlq",req.value,{validateStatus: function (status) {
-    return true; 
-}}).then(r=>{
-    if(r.status===200){
-        data.value=r.data.data
-        if(data.value===null){
-            data.value=[]
+const post = () => {
+  http
+    .post("/p/user/hlq", req.value, {
+      validateStatus: function (status) {
+        return true;
+      },
+    })
+    .then((r) => {
+      if (r.status === 200) {
+        data.value = r.data.data;
+        if (data.value === null) {
+          data.value = [];
         }
-    }else{
-        createToast("Failed to fetch data: "+r.data.msg)
-    }
-})
-}
-onMounted(post)
-const find = (e)=>{
-    e.preventDefault()
-    post()
-}
-const columns=createColumns()
-const pagination={
-    pagiSize:20,
-}
+      } else {
+        createToast("Failed to fetch data: " + r.data.msg);
+      }
+    });
+};
+onMounted(post);
+const find = (e) => {
+  e.preventDefault();
+  post();
+};
+const columns = createColumns();
+const pagination = {
+  pagiSize: 20,
+};
 // defineComponent({
 //         setup(){
 //             const message = useMessage()
@@ -80,5 +84,16 @@ const pagination={
 //             }
 //         }
 //     })
-
 </script>
+
+<style lang="scss">
+.centerform {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.n-input:not(.n-input--autosize) {
+    width: 500px;
+}
+</style>
